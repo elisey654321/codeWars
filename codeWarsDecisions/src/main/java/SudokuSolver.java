062961c42fa6e37df1e27e71a39ae4583645ce99
elisey654321 <elisey654321@mail.ru>
2022-09-19 22:55:26 +0300

    step 2

--- codeWarsDecisions/src/main/java/SudokuSolver.java
+++ codeWarsDecisions/src/main/java/SudokuSolver.java
@@ -4,6 +4,7 @@
 */
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SudokuSolver {
@@ -25,36 +26,66 @@ public class SudokuSolver {
                 grid9x3x3[nowGrid][nowJ][nowI] = grid[i][j];
                 if (grid[i][j] != 0) gridNoZero[nowGrid]++;
 
-                if (nowI == 2){
+                if (nowI == 2) {
                     nowGrid++;
                     nowI = 0;
-                }else nowI++;
+                } else nowI++;
             }
-            if ((j+1)%3 == 0){
+            if ((j + 1) % 3 == 0) {
                 lastGrid = nowGrid;
-            } else if (nowJ == 2){
+            } else if (nowJ == 2) {
                 nowJ = 0;
                 nowGrid = lastGrid;
-            }else {
+            } else {
                 nowJ++;
                 nowGrid = lastGrid;
             }
-        }
-
-        for (int i = 0; i < grid9x3x3.length; i++) {
-            for (int j = 0; j < grid9x3x3[i].length; j++) {
-                for (int k = 0; k < grid9x3x3[i][j].length; k++) {
-                    System.out.printf(String.valueOf(grid9x3x3[i][j][k]));
-                }
-                System.out.println();
-            }
-            System.out.println();
         }
 
     }
 
     public int[][] solve() {
 
+        for (int i = 0; i < grid9x3x3.length; i++) {
+            for (int j = 0; j < grid9x3x3[i].length; j++) {
+                for (int k = 0; k < grid9x3x3[i][j].length; k++) {
+                    int result = checkConst(i, j, k);
+//                    System.out.println(result);
+                }
+//                System.out.println();
+            }
+//            System.out.println();
+        }
+
         return null;
     }
+
+    private int checkConst(int i, int j, int k) {
+        System.out.printf(String.valueOf(grid9x3x3[i][j][k]));
+        ArrayList<Integer> lineJ = new ArrayList<Integer>();
+        ArrayList<Integer> lineK = new ArrayList<Integer>();
+
+        int iK = (i - (i + 1) % 3 == -1 ? 0 : i - (i + 1) % 3);
+        int iJ = 0;
+
+
+        for (int l = 0; l < 3; l++) {
+            iK += l;
+            for (int m = 0; m < 3; m++) {
+                Integer number = grid9x3x3[iK][j][m];
+                if (number != 0) lineK.add(number);
+            }
+        }
+
+        for (int l = 0; l < 3; l++) {
+            iJ += (3 * l - 1 == -1 ? 0 : 3 * l - 1);
+            for (int m = 0; m < 3; m++) {
+                Integer number = grid9x3x3[iK][m][l];
+                if (number != 0) lineK.add(number);
+            }
+        }
+
+        return 0;
+    }
+
 }
